875a95d0ca85579d644dda72ecba58758b06992a
neerzallaaa <neerzallaaa@gmail.com>
2023-03-14 20:41:54 +0700

    mengumpulkan exercise 1

---
+++ Exercise-01.py
@@ -0,0 +1,15 @@
+#Exercise 01
+
+print ("=================================================================================================================================================================================================================")
+Nama = "Neerzalla Atha Nafisah"
+Nim = "I0322094"
+Visualisasi = "Saya berasal dari Kota Bekasi. Kota Bekasi merupakan kota yang panas dan sering terjadi kemacetan."
+Harapan = "Harapan saya ketika menjadi lulusan Teknik Industri UNS adalah saya dapat mengaplikasikan hal-hal yang sudah dipelajari dan dapat membantu masyarakat sekitar dengan hal-hal yang saya aplikasikan tersebut."
+print ("=================================================================================================================================================================================================================")
+
+print("Halo Semua!")
+print("Perkenalkan nama saya", Nama)
+print("NIM saya", Nim)
+print(Visualisasi)
+print(Harapan)
+print("Terima kasih!")
